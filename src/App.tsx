c0869442529e89c6cc3a5e91a1dd2328bc66deb3
Kokosalah45 <kokp.amged@gmail.com>
2022-06-14 19:11:35 +0200

    added zustand formatted code with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header font-bold">
+    <div className='App'>
+      <header className='App-header font-bold'>
         <p>Hello Vite + React!</p>
       </header>
     </div>
