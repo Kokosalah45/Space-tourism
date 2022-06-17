af2c4771f65b31845633e958600c7bcb105d2bd3
Kokosalah45 <kokp.amged@gmail.com>
2022-06-17 14:40:23 +0200

    created a cloud database and installed prisma for interfacing with the database

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,15 @@ app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify({ a: 1 }, null, 3));
 });
-app.listen(3001, () => console.log('Example app listening on port 3001!'));
+app.get('/2', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ a: 2 }, null, 3));
+});
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+app.get('/3', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ a: 3 }, null, 3));
+});
+app.listen(3000, () => console.log('Example app listening on port 3001!'));
